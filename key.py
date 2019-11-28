6dcecc61b46aec8a477c36b49480ae2de4c022c2
c1ewd <c1ewd@protonmail.com>
2019-11-28 22:34:14 +0500

    Test RSA encrypt/decrypt

--- key.py
+++ key.py
@@ -1,6 +1,8 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 private_key = rsa.generate_private_key(
     public_exponent=65537,
@@ -21,34 +23,34 @@ for char in private_pem:
 
 bytes_private_pem = b"""
 -----BEGIN ENCRYPTED PRIVATE KEY-----
-MIIFLTBXBgkqhkiG9w0BBQ0wSjApBgkqhkiG9w0BBQwwHAQI8OGwhBSDAbkCAggA
-MAwGCCqGSIb3DQIJBQAwHQYJYIZIAWUDBAEqBBB93f6JsDIWHFMfGjyb1o1dBIIE
-0CPTxQcsRY8POi/1ZII1zmp2++j7ES3RHsY2IWfboUW4QkROGM5cumKsRjfvCHsX
-2VFVpNY0HjYvmFM3nxaWT1dvOIqI89qQKSvnDdY2xw/ba0/B9Or1F98J9NX+y/mR
-JAX0m64+9C2gvfFQrpOjuTM4IBZOaXnjr3AwM0X9FkksBcRFHBD4EXrUJUi6prL9
-urJ0UBqmZ965BM1JBCtOM31zPBvlfVd/1mO1v2OVfjRD6XX6SMRxbLd++u6pYF5A
-odm6mB1Vo56Xdh5x+hYPIJ5z90bj3M2ijxlt4NMKzRpjhP3QaXkHnMExziTRKG5Z
-kOLIiG3g1FyqRDOajLvNKE6cOlTpFai+GxSIE+Z9k1jGJhKz7ZIyyek1e99oGyfq
-cqdTKkt/W3rZhyoyqyrYNrazzSIj8lmtCW92pOMpEmgx5D7fIPzS9agkyiQNLWJ/
-cEQaknv9pPk6lhT04TbdCC0ff89iFGoHLY7dmza6YzmGWvdyjGGCTe8Nv2mxMFaw
-/kCKJB1Das5EZVKD0YvtIBB/2dC/VkiXWoku19WJUx4yxytKlHf/6Pat+GgOqfi3
-BAVpmRj063QFUP6W/o4Bam21iChN5oXqDAxpDOTl9fxbINeYYsw90OdzJmQa446B
-Bp/e/HRCMOb0dwjXm596Sq3wTBeaKt8rD5se8esNrcC8qukxfY5pr9/ABiSXe7sA
-jCwOPryFv5AbsMGf4sYkv0qBjX+P2RCElX2eu1SAzh6g2mERtFg4XpukIuNsoh/t
-GafTWemiQaJQ7RFTDo94Ibx8gIhwMQokkNTPUCh426aGHyiIA7Xw2hFp+U2rnT50
-CkF5LwBa/8j09/dqkVMy3wv6V9LPOMBQmWG4NeyA66BQHtZ2mTqQVE91sXnmsjCo
-pUnj9D3nA/zVBeOdnoRMGIEr6MOgS3TL0OT91gfD6928BXH7zWf6uZRYxbv0Oow+
-KN4cvAMd7biQy248uID7X0bQbJ067OOJWAxcDaItBlJsBGiTDHOE4m6UJCb6EuLI
-8FI1OSYNRkTMaD9SO1ahGOae+fYae2dyeyLu6fRP/oUa0Q5DXL2EskvK+iv2XmtR
-lchXFQMvNwF0qgq3D9ytViZuL42yue++WIG/CEiIG2QXKMc7zYyFYCWrW3AxslSc
-hf4rpxsCML+SnoEzJg6WoDqepecI0sK+OHLsU7mRqAGoXehEba4/QHbXWnhlHc4h
-0ORUuEzyRINnuc/mC1Ap3SwxDe55PQbkaRZppQPZr41Ya8CdPjI8Sy+3RwT7waKh
-4sY3+hVwal5ooRzwwiCa1WtLc/CvysP0zyQ4tA0q2hRcEraMtq0uRSUGj2ObEHDF
-QwcpvdEeOi1I/n/cZpEZ5HaQo9RVZj/o3GK20u0BIV3KwehA6dpP5CcPB59MKI1t
-eOCSTCZ9BjKodijQR/+7eveJCJ62E1rUUFsKAOkzYAxdYU/r8vzsTYdsPVigjPPv
-0W8Xg8fBTSBbsPEeDiWOMnWBFCOIxCWl7itc0nzIuIwB2QydXVfKLKxbr4WgsQpX
-+35gYDhAlFBJlA+wi6EyDjlkZoC6BB2wpxOIJEnT03+Hf8Q8dAAxDhAlPA5lp1J+
-QHH6eLQG2u8ucn+Wr82/BBk09+WUZE1X87cgo6xLrnvk
+MIIFLTBXBgkqhkiG9w0BBQ0wSjApBgkqhkiG9w0BBQwwHAQIUbLn4YoBeYgCAggA
+MAwGCCqGSIb3DQIJBQAwHQYJYIZIAWUDBAEqBBCL87wS822SvvuxBlS8QsFdBIIE
+0MIJFRE7yJYZrObhjY1ntvBpgBvY4X7I9irMCuQiMDnF9pzizBjMBdvfCPcgMZXa
+nv9OKkiOcrfk+cMTRfTCi0jYIvOLwIiX0JmMeloE6xml6OZIcZbJdclpbqYXIeS/
+dALMyEhUTrc6zA/MR337Bo5zFD+WJdtLCwhWeKK4IjoHxJws9JPlEhDw3Zm0ihrp
+WgM2RMEpGNPO3p2301syai1bY12x3WxvQyGJHhl8rdDHfpi2s3M4qW7aBQ5GWAVb
+l7vsTJCUaBj44wDF2fDyA5XsjvQoYQWZ8MdbA9QI8zzWrLkr7TBqw22O57JYpx7q
+y4dXcr4+3+15daQ0fuhsVcfRJEH+l7GS3Bx0tB5/nwhsvYwbHZ2SB90JlwPYnEbR
+kiTs8hZm4mAooxXGfs4pF+l3OL6fcaMzXiJlOj0YsY1aWnXRArB3z3NPT4T1iq2a
+u6osLd95Rg2OFD2SKSpMwW4GgQaRivA11CUuOZ1SoNXOmgaiDTU/vFMQ5cOV6HCN
+kz0XoOHx5i0RM4q/thId0rZLGTIsEZxM4imYwllxSljczoGV4bgimG/lIn5uMGrl
+KEhNyUjhsGDpedvKbpKm7KH90ckfA2aj8Om1G+fyR0Et7Y7qk3tgHKSJv1IzI6/V
+uimtl0MUmn+EFDp+xi6QSHc0JQxcHzJBDp2/OCJc8hhFAWyFRv1G1IuJzk4u62Xq
+9NNoymLG9bE/0jL/YOVdT/IxbAkTr4fNYNrHZqD8Y8HiAL/ri+x1WagY2PqwkLl2
+xBWK79IFB93UfILEILTeO9x4zc7NQeu9bwOkmbwj7laP6WmnLgxnhAVgU6WZ58iV
+gS+nLZTraqIYgTWkbmtuD0XmCWgimicF4Levcy2TiYm54t4EqEXJCfitYUfIYuF4
+gzfD3zI9dkswLAemBCGCY1x4AMxoWsLSjyZ4MgKle+ctYUsaudQXOcu9Cp+x+TEW
+nfNKWhCqrStI7FyGTM8Fvf0mnu6EjT9YSZA+ehZPS3SCjzDmYmSShNUo5WxnVox6
+k/9jDu5lfAV5dZDxZATap1SfVnlXE+oHoAlI4aVIRV1rA3HY9YW9Lvpoa9XRdqov
+7OLkq43aHAyCiL+OT+uHCZGMKx711N5MRVxSg/bH8qS51Q4/F3sDoM5QPYC85UaG
+R1D3NA2u0BvQWweJ7xuM/k/dwz3hXfokAftH9jy/GcsbOTKq0I8VaE0svM9LeSeY
+r2Lw3vTWGLd6llitymPbdIOhLCUfUrn9ZNe1JUEUtiAbcNfspoq6fWJoarIN3Fdg
+DE1rguszmTrq8Mj9NXK7EZ71LzUm238+gT/A/O8kpCvl6b/9XihqBzeQv6FA+Fk6
+5FRMtlXpf9w5tO1nvxxPFIcH5m6iMd4oUp/mPdeTW1DQ0hYQ3V+vW3Vw00M/Ca9J
+JnCqjA9e0kt1pcA4NhSMBl/wBZydpAuKemX2QdtSgaP/G8cHGH3i3ff4FH93tA0J
+DxgZAhCMxplk2oZWPU2QdTjgLdlrfUrTk0ANzivJnbVX4Aweosm4+MymUJdQEH7k
+zqzt+g2L4xcw3TgCxe03ssw6S77rOd86CuxjckMncokKqLxX6pNMlVT3hZkHparZ
+Fp37ziYQQfuOfjYdx24q1ppySSv8UU8YX9xmV84BJkk9
 -----END ENCRYPTED PRIVATE KEY-----
 """
 
@@ -57,6 +59,18 @@ loaded_private_key = serialization.load_pem_private_key(
     password=b'bestmessenger',
     backend=default_backend()
 )
+
+message = b"A message I want to sign"
+signature = loaded_private_key.sign(
+    message,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)
+
+#print('Signature: ' + signature)
 
 #print('Private pem: ' + str(loaded_private_key))
 
@@ -74,13 +88,13 @@ for char in public_pem:
 
 bytes_public_pem = b"""
 -----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmYiDpFTxp/k5x6+7aXaf
-bnlN7kP4EM2+wUZ/D3bE4rUB4oASixuQhuM4/H3q4+2WlPlXDc4gytSFPR3OGhoE
-e3ajKZowfkSiPHWos237AzTNoTd4vyfXApheX/AEM8aqCMeIfdSjzL+92HUFWDvn
-f5r7UrG8rZjdD9xudKlk56O2U5717uFZAMvQ8Mg5/owzOEYhTmOQ+DRqZp9ATwwt
-8fAw4ZjNqYcR+SW6p7zclPkg+f9+VittW4uiM6OW8cMqEIgG84dLlpfoGhQ+0rfE
-he6NHPYyISCQGSAanc1957NWcKaDCUqgW4Y3frem+JhqP/JmBAhE9OrJTnt13HjI
-cwIDAQAB
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2FFIfmrJDYJi7UZRXnd1
+cCwidyA9JIxUecqMmIhdJGtSfdoXDCvLiiyJc/zMml0DAw9bD7Lu8tDqHHLTM4mo
+2A/6Y8WkYdYSz5VNKSjWqm8TEFbMoWbUGrg32o0tQwJvO+kl0dnTnoGzJKr9GwZI
+gfvbiqZHDALPt/QBk/Rn6PfI4QUpfdkSuEVGUKlHSM8sRfUQjgFKrubRBl1U2P1T
+JReVChDe637UEaD6+v9Otn0cwdDPdw4AhVCf0xkMQAtzON58xPfk1MKSuw57ymjw
+FBF3fC5wIkTGmOYJWdaNtxnnIKTXSIhOBWUVkGeHhE7RBHHSaH1fjs4XhD3+BK11
+GwIDAQAB
 -----END PUBLIC KEY-----
 """
 
@@ -88,3 +102,38 @@ loaded_public_key = serialization.load_pem_public_key(
     bytes_public_pem,
     backend=default_backend()
 )
+
+try:
+    loaded_public_key.verify(
+        signature,
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    ciphertext = loaded_public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf = padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm = hashes.SHA256(),
+            label = None
+        )
+    )
+
+    #print(ciphertext)
+except:
+    print('Invalid signature')
+
+plaintext = loaded_private_key.decrypt(
+    ciphertext,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+plaintext == message
+print(plaintext)
